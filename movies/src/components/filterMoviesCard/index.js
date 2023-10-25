575b436e563339aa3c2174ef51362b66629e638b
Jia Yang <20104736@mail.wit.ie>
2023-10-25 23:38:43 +0100

    Fetching genre list from API.

--- movies/src/components/filterMoviesCard/index.js
+++ movies/src/components/filterMoviesCard/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Card from "@mui/material/Card";
 import CardMedia from "@mui/material/CardMedia";
 import CardContent from "@mui/material/CardContent";
@@ -12,78 +12,106 @@ import Select from "@mui/material/Select";
 import img from '../../images/pexels-dziana-hasanbekava-5480827.jpg'
 
 // 定义按类型搜索表单控件的样式
-const formControl = 
-  {
+const formControl =
+{
     margin: 1,
     minWidth: 220,
     backgroundColor: "rgb(255, 255, 255)"
-  };
+};
 
 export default function FilterMoviesCard(props) {
 
     // 局部数组，包含三种电影类型
-  const genres = [
-    {id: 1, name: "Animation"},
-    {id: 2, name: "Comedy"},
-    {id: 3, name: "Thriller"}
-  ]
+    const [genres, setGenres] = useState([{ id: '0', name: "All" }])
 
-  return (
-    // 卡片组件
-    <Card 
-      sx={{
-        maxWidth: 345,
-        backgroundColor: "rgb(204, 204, 0)"
-      }} 
-      variant="outlined">
+    useEffect(() => {
+        fetch(
+            "https://api.themoviedb.org/3/genre/movie/list?api_key=" +
+            process.env.REACT_APP_TMDB_KEY
+        )
+            .then(res => res.json())
+            .then(json => {
+                // console.log(json.genres) 
+                return json.genres
+            })
+            .then(apiGenres => {
+                setGenres([genres[0], ...apiGenres]);
+            });
+        // eslint-disable-next-line
+    }, []);
 
-      <CardContent>
+    const handleChange = (e, type, value) => {
+        e.preventDefault()
+        // Completed later
+    };
+    const handleTextChange = e => {
+        handleChange(e, "name", e.target.value)
+    }
+    const handleGenreChange = e => {
+        handleChange(e, "genre", e.target.value)
+    };
 
-        <Typography variant="h5" component="h1">
-            {/* 搜索图标 */}
-          <SearchIcon fontSize="large" />
-          Filter the movies.
-        </Typography>
-        {/* 允许搜索关键词 */}
-        <TextField
-          sx={{...formControl}}
-          id="filled-search"
-          label="Search field"
-          type="search"
-          variant="filled"
-        />
-        {/* 下拉选择器，每个类型是一个MenuItem，由genres数组动态映射 */}
-        <FormControl sx={{...formControl}}>
-          <InputLabel id="genre-label">Genre</InputLabel>
-          <Select
-            labelId="genre-label"
-            id="genre-select"
-          >
-            {/* 动态映射数组 */}
-            {genres.map((genre) => {
-              return (
-                <MenuItem key={genre.id} value={genre.id}>
-                  {genre.name}
-                </MenuItem>
-              );
-            })}
-          </Select>
-        </FormControl>
-      </CardContent>
-      {/* 导入图片 */}
-      <CardMedia
-        sx={{ height: 300 }}
-        image={img}
-        title="Filter"
-      />
+    return (
+        // 卡片组件
+        <Card
+            sx={{
+                maxWidth: 345,
+                backgroundColor: "rgb(204, 204, 0)"
+            }}
+            variant="outlined">
 
-      <CardContent>
-        <Typography variant="h5" component="h1">
-          <SearchIcon fontSize="large" />
-          Filter the movies.
-          <br />
-        </Typography>
-      </CardContent>
-    </Card>
-  );
+            <CardContent>
+
+                <Typography variant="h5" component="h1">
+                    {/* 搜索图标 */}
+                    <SearchIcon fontSize="large" />
+                    Filter the movies.
+                </Typography>
+                {/* 允许搜索关键词 */}
+                <TextField
+                    sx={{ ...formControl }}
+                    id="filled-search"
+                    label="Search field"
+                    type="search"
+                    variant="filled"
+                    value={props.titleFilter}
+                    onChange={handleTextChange}
+                />
+                {/* 下拉选择器，每个类型是一个MenuItem，由genres数组动态映射 */}
+                <FormControl sx={{ ...formControl }}>
+                    <InputLabel id="genre-label">Genre</InputLabel>
+                    <Select
+                        labelId="genre-label"
+                        id="genre-select"
+                        defaultValue=""
+                        value={props.genreFilter}
+                        onChange={handleGenreChange}
+                    >
+                        {/* 动态映射数组 */}
+                        {genres.map((genre) => {
+                            return (
+                                <MenuItem key={genre.id} value={genre.id}>
+                                    {genre.name}
+                                </MenuItem>
+                            );
+                        })}
+                    </Select>
+                </FormControl>
+            </CardContent>
+            {/* 导入图片 */}
+            <CardMedia
+                sx={{ height: 300 }}
+                image={img}
+                title="Filter"
+            />
+
+            <CardContent>
+                <Typography variant="h5" component="h1">
+                    <SearchIcon fontSize="large" />
+                    Filter the movies.
+                    <br />
+                </Typography>
+            </CardContent>
+        </Card>
+    );
 }
