d9f2b06c723d95e2c39b8710aee980664916ab06
Jia Yang <20104736@mail.wit.ie>
2023-10-26 16:28:29 +0100

    E2E tests - Exercise - Combined title and genre filtering tested.

--- movies/cypress/e2e/filtering.cy.js
+++ movies/cypress/e2e/filtering.cy.js
@@ -1,4 +1,4 @@
-import { filterByGenre, filterByTitle } from "../support/e2e";
+import { filterByGenre, filterByTitle, filterByGenreAndTitle} from "../support/e2e";
 
 let movies; // List of Discover movies from TMDB
 
@@ -40,8 +40,8 @@ describe("Filtering", () => {
     });
     describe("By movie genre", () => {
         it("show movies with the selected genre", () => {
-            const selectedGenreId = 35;
-            const selectedGenreText = "Comedy";
+            const selectedGenreId = 18;
+            const selectedGenreText = "Drama";
             const matchingMovies = filterByGenre(movies, selectedGenreId);
             cy.get("#genre-select").click();
             cy.get("li").contains(selectedGenreText).click();
@@ -56,5 +56,21 @@ describe("Filtering", () => {
     });
     describe("Combined genre and title", () => {
         // TODO
+        it("show movies with the selected genre and with 'm' in the title",() =>{
+            const searchString = "p";
+            const selectedGenreId = 18;
+            const selectedGenreText = "Drama";
+            const matchingMovies = filterByGenreAndTitle(movies, selectedGenreId, searchString);
+            cy.get("#genre-select").click();
+            cy.get("li").contains(selectedGenreText).click();
+            cy.get("#filled-search").clear().type(searchString); // Enter m in text box
+            cy.get(".MuiCardHeader-content").should(
+                "have.length",
+                matchingMovies.length
+            );
+            cy.get(".MuiCardHeader-content").each(($card, index) => {
+                cy.wrap($card).find("p").contains(matchingMovies[index].title);
+            });
+        })
     });
 });
--- movies/cypress/support/e2e.js
+++ movies/cypress/support/e2e.js
@@ -1,5 +1,8 @@
 export const filterByTitle = (movieList, string) =>
-  movieList.filter((m) => m.title.toLowerCase().search(string) !== -1);
+    movieList.filter((m) => m.title.toLowerCase().search(string) !== -1);
 
 export const filterByGenre = (movieList, genreId) =>
-  movieList.filter((m) => m.genre_ids.includes(genreId));
+    movieList.filter((m) => m.genre_ids.includes(genreId));
+
+export const filterByGenreAndTitle = (movieList, genreId, string) =>
+    movieList.filter((m) => (m.genre_ids.includes(genreId) && m.title.toLowerCase().search(string) !== -1));
